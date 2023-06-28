9dce4697163488f6326236df33a07bf6735eb3ca
George - David Apostolidis <iis20001@uom.edu.gr>
2023-06-28 15:13:12 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import re
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def find_metadata_div(source_code_to_search):
+    print("Finding metadata div...")
+    soup = BeautifulSoup(source_code_to_search, 'html.parser')
+    metadata_div = soup.find('div', class_='groups')
+    metadata_div.prettify()
+
+    return metadata_div.prettify()
+
+
+def get_page_source(page_url):
+    driver = webdriver.Firefox()
+    driver.get(page_url)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'groups')))
+    page_source = driver.page_source
+    page_source = page_source.encode('utf-8')
+    driver.quit()
+
+    return page_source
+
+
+if __name__ == '__main__':
+    url = 'https://www.sciencedirect.com/science/article/pii/S2665963823000283'
+    source_code = get_page_source(url)
+    print("=====================================================")
+    metadata_div = find_metadata_div(source_code)
+    print("Metadata Div: \n", metadata_div)
+    pattern_version = r'Current code version<\/td>\s*<td class="align-left">(.*?)<\/td>'
+    version_match = re.search(pattern_version, metadata_div, re.DOTALL)
+    if version_match:
+        print("Version: ", version_match.group(1))
+    else:
+        print("Version: Not found")
+    print("=====================================================")
